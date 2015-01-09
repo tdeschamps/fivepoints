cb3a3f36041ae42ced87ba0b445cfdbfc29e23c3
tdeschamps <thomasdscmps@gmail.com>
2015-01-09 23:25:54 +0100

    completion working

--- app/assets/javascripts/city_guides/city_guide_places.js
+++ app/assets/javascripts/city_guides/city_guide_places.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	$('#add-place').click(function() {
 		var nb_places = $("tbody > tr").length;
 		var input = "<input id="+"user_input_foursquare"+" name="+"user_input_autocomplete_address"+" class="+"form-control"+" placeholder="+"Start typing your address..."+">"
+		
 		$('#sortable tbody').append("<tr  class="+"item"+"><td>toto</td><td>"+input+"</td></tr>");
 		if (nb_places >= 4) {
 			$('#add-place').hide();
