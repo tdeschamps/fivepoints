ff91a65e609bf5d7b18636bb95e7866b18780cd8
tdeschamps <thomasdscmps@gmail.com>
2015-01-28 17:34:16 +0100

    Switching branch

--- app/services/foursquare_picture.rb
+++ app/services/foursquare_picture.rb
@@ -9,14 +9,17 @@ class FoursquarePicture
 		@place_id = place_id
 	end
 
-	def GetAdditionalInfos
+	def get_additional_infos
+
 		@foursquare_place_id = Place.find(@place_id).foursquare_id
 		response = open("https://api.foursquare.com/v2/venues/#{@foursquare_place_id}?&client_id=#{@foursquare_id}&client_secret=#{@foursquare_secret}&v=20140806")
 		json = JSON.parse(response.read.to_s)
 		picture_object = json['response']['venue']['photos']['groups'][0]['items'][0]
 		foursquare_rating = json['response']['venue']['rating']
 		picture_url = "#{picture_object['prefix']}#{picture_object['width']}x#{picture_object['height']}#{picture_object['suffix']}"
+		
 		return {foursquare_picture_url: picture_url, foursquare_rating: foursquare_rating}
+	
 	end 
 
 end
