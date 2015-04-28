d1b00d2dc1e155352361c22d3d7815c6fba966e0
tdeschamps <thomasdscmps@gmail.com>
2015-04-28 13:17:26 +0200

    tutu

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = ENV['CLOUDFRONT_ENDPOINT']
   config.static_cache_control = "public, max-age=31536000"
+  config.font_assets.origin = 'http://fivemarks.in'
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
