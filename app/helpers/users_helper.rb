f027b0187dfa16ad1a60fd1f48ba944b3475d1fa
tdeschamps <thomasdscmps@gmail.com>
2015-05-01 18:07:20 +0200

    add a user profile picture helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+	
+	def choose_user_picture(user)
+
+		if user.uploaded_files.last
+			user.uploaded_files.last.file
+		elsif user.image
+			user.image
+		else
+			'default_user_picture.png'
+		end					
+	end	
 end
