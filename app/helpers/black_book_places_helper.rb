7f54a9d9c7e6d260b441ef50e4904bf6dcc8b2cf
tdeschamps <thomasdscmps@gmail.com>
2015-05-13 18:52:13 +0200

    fix typo'

--- app/helpers/black_book_places_helper.rb
+++ app/helpers/black_book_places_helper.rb
@@ -2,11 +2,11 @@ module BlackBookPlacesHelper
 	def choose_black_book_place_picture(black_book_place)
 
 		if black_book_place.uploaded_files.last
-			"background-image: url(#{black_book_place.uploaded_files.last.file.url(:large)});"
+			black_book_place.uploaded_files.last.file.url(:large)
 		elsif black_book_place.place.foursquare_picture_url
-			"background-image: url(#{black_book_place.place.foursquare_picture_url});"
+			black_book_place.place.foursquare_picture_url
 		else
-			"background-color: #{%w(lightpink royalblue lightblue lightgreen lightcyan lightsteelblue).sample};"
+			false
 		end					
 	end	
 end
