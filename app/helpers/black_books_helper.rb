e601f69765806a8dd11b975d87f2c3f89c0c0776
tdeschamps <thomasdscmps@gmail.com>
2015-06-09 08:22:46 +0200

    fixed typo

--- app/helpers/black_books_helper.rb
+++ app/helpers/black_books_helper.rb
@@ -1,5 +1,5 @@
 module BlackBooksHelper
 	def facebook_black_book_url_render(resource)
-		black_book_url(resource).gsub(/[\:\/]/,":" => "%3F", "/" => "%2F")
+		black_book_url(resource).gsub(/[\:\/]/,":" => "%3A", "/" => "%2F")
 	end
 end
