7a875823cea6207888e8514c5fa86ad0aae9186e
tdeschamps <thomasdscmps@gmail.com>
2015-09-20 19:59:15 +0200

    handle places prices

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,2 +1,10 @@
 module PlacesHelper
+	
+	def render_price(place)
+		if price = place.price
+			price_tag = ''
+			price.times {price_tag = price_tag + '<i class="fa fa-usd dollar-sign"></i>'}
+			('<div class="attributes"><strong>Price: </strong><p class="text-truncate">' + price_tag + '</p><div class="clear"></div></div>').html_safe
+		end
+	end
 end
--- app/services/foursquare_infos.rb
+++ app/services/foursquare_infos.rb
@@ -7,6 +7,7 @@ class FoursquareInfos
 		@foursquare_id = ENV['FOURSQ_CLIENT_ID']
 		@foursquare_secret = ENV['FOURSQ_CLIENT_SECRET']
 		@place = place
+		@venue = nil
 	end
 
 	def get_additional_infos
@@ -15,34 +16,20 @@ class FoursquareInfos
 		foursquare_api_version = DateTime.now().strftime("%Y%m%d")
 
 		foursquare_place_url = "https://api.foursquare.com/v2/venues/#{foursquare_place_id}?&client_id=#{@foursquare_id}&client_secret=#{@foursquare_secret}&v=#{foursquare_api_version}&locale=en"
-		p foursquare_place_url
 		response = open(foursquare_place_url)
 		#p response
-		venue = JSON.parse(response.read.to_s)['response']['venue']
+		@venue = JSON.parse(response.read.to_s)['response']['venue']
 		
-		if venue['photos']['count'] > 0
-			picture_group = venue['photos']['groups'][0]['items']
-			
-			
-			picture_group.each do |picture|
-      			picture_object = picture 
-      			picture_url = "#{picture_object['prefix']}#{picture_object['width']}x#{picture_object['height']}#{picture_object['suffix']}"
-      			FoursquarePicture.perform_later place_id, picture_url
-    		end
-
-			picture_object = picture_group[0] 
-			foursquare_picture_url = "#{picture_object['prefix']}#{picture_object['width']}x#{picture_object['height']}#{picture_object['suffix']}"
-			
-		end
+		foursquare_picture_url = handle_pictures
 		
-		foursquare_rating = venue['rating']
+		foursquare_rating = @venue['rating']
 		social_infos = {}
-		venue['contact'].each do |k,v| 
+		@venue['contact'].each do |k,v| 
 			social_infos[k.underscore.to_sym] = v
 		end
 
 
-		location = venue['location']
+		location = @venue['location']
 		
 		location_infos = {
 			address: location['address'],
@@ -53,21 +40,47 @@ class FoursquareInfos
 			state: location['state']
 		}
 
+		price_info = {}
+		if @venue['price']
+			price_info = {price: @venue['price']['tier'],
+						price_description: @venue['price']['message']
+			}
+		end
+		
 
-		categories = venue['categories']
+
+		categories = @venue['categories']
 		
 		categories.each do |category|
 			FoursquareCategory.perform_later place_id, category
 		end
 
-		if venue['description']
-			description = venue['description']
+		if @venue['description']
+			description = @venue['description']
 		end	
 
 		return {foursquare_picture_url: foursquare_picture_url, 
 				foursquare_rating: foursquare_rating, 
-				description: description}.merge(social_infos).merge(location_infos)
+				description: description}.merge(social_infos).merge(location_infos).merge(price_info)
 	
 	end 
+
+	def handle_pictures
+		pictures = @place.uploaded_files.map(&:file_file_name)
+		if @venue['photos']['count'] > 0
+			picture_group = @venue['photos']['groups'][0]['items']
+			
+			
+			picture_group.each do |picture|
+      			picture_object = picture 
+      			picture_url = "#{picture_object['prefix']}#{picture_object['width']}x#{picture_object['height']}#{picture_object['suffix']}"
+      			FoursquarePicture.perform_later @place.id, picture_url unless pictures.include? picture_object['suffix'].gsub('/','')
+    		end
+
+			picture_object = picture_group[0] 
+			return "#{picture_object['prefix']}#{picture_object['width']}x#{picture_object['height']}#{picture_object['suffix']}"
+			
+		end
+	end
 
 end
